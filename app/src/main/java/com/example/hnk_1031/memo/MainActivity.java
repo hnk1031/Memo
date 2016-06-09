0969e498fdf091f1055ad003d31626711e90e1bb
hnk1031 <v8gjyno9fl@gmail.com>
2016-06-09 21:39:28 +0900

    memo

---
+++ app/src/main/java/com/example/hnk_1031/memo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hnk_1031.memo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    public void memo(View v){
+        Intent intent = new Intent(this,MemoActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
